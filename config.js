b0702f76f5a6ff7a4a2913e8bf4c821c469b736b
KingCoderGithub <84721340+KingCoderGithub@users.noreply.github.com>
2022-01-01 12:24:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCg1lEFmv_kOboeI-zc_GAuAsanKdjrG58",
+    authDomain: "project71e-ride.firebaseapp.com",
+    projectId: "project71e-ride",
+    storageBucket: "project71e-ride.appspot.com",
+    messagingSenderId: "938987586219",
+    appId: "1:938987586219:web:a66fb1afe9ba4dbae9adee"
+  };
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
